4b4584fd3ad89b6a6531731c48034942820ac646
Leandro Rodrigues <leandroleo02@gmail.com>
2021-11-02 14:15:47 -0300

    Fix unit tests

--- src/test/kotlin/com/example/notes/infrastructure/CsvNotesRepositoryTest.kt
+++ src/test/kotlin/com/example/notes/infrastructure/CsvNotesRepositoryTest.kt
@@ -17,15 +17,15 @@ class CsvNotesRepositoryTest {
 
     @Test
     fun findNoteById() {
-        val note = csvNotesRepository.findById("5")
+        val note = csvNotesRepository.findById("6175f4b9d75e0c0d5cda0f35")
         assertThat(note).isNotNull
     }
 
     @Test
     fun findNoteWithAllValues() {
-        val note = csvNotesRepository.findById("5")
+        val note = csvNotesRepository.findById("6175f4b9d75e0c0d5cda0f35")
         assertThat(note)
             .usingRecursiveComparison()
-            .isEqualTo(Note("5", "Cold", "white", "Winter is coming!"))
+            .isEqualTo(Note("6175f4b9d75e0c0d5cda0f35", "Cold", "white", "Winter is coming!"))
     }
 }
